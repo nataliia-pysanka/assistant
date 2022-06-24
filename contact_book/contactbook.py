66c7b0b0f9a2d2d9f80277329056682c7418ae0c
nataliia-pysanka <pysankanataliia@gmail.com>
2022-06-25 01:26:15 +0300

    search/display_all/__next__ and some tests

--- contact_book/contactbook.py
+++ contact_book/contactbook.py
@@ -1,5 +1,8 @@
 from collections import UserDict
 from contact_book.record import Record
+from faker import Faker
+from datetime import datetime
+from random import randint
 
 
 class ContactBook(UserDict):
@@ -26,7 +29,8 @@ class ContactBook(UserDict):
         if len(self.data) > 0:
             for key in self.data:
                 res += f'{key}\n'
-        return res
+            return res
+        return None
 
     def delete(self, key: str) -> bool:
         """
@@ -39,3 +43,76 @@ class ContactBook(UserDict):
             self.names.remove(key)
             return True
         return False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.counter < len(self.names):
+            if self.counter > 0 and self.counter % 3 == 0:
+                input()
+            index = self.counter
+            self.counter += 1
+            return self.data[self.names[index]]
+
+        self.counter = 0
+        raise StopIteration
+
+    def search(self, name: str) -> Record:
+        """
+        Search record in storage by name
+        :param name: str
+        :return: Record
+        """
+        if name in self.names:
+            rec = self.data.get(name)
+            print(rec)
+        else:
+            print(f'No name {name} in contacts')
+
+    def display_all(self):
+        """
+        Display all  records
+        :return: None
+        """
+        for record in self:
+            print(record)
+
+
+# class Record:
+#     """
+#     Class for instance Record
+#     """
+#
+#     def __init__(self, name, birthday, phone):
+#         self.name = name
+#         self.birthday = birthday
+#         self.phone = phone
+#
+#     def __str__(self):
+#         rec = '\t {:<8}...{:<15}'.format('........', '...............') + '\n'
+#         rec += '\t {:<8} : {:<15}'.format('Name', self.name) + '\n'
+#         rec += '\t {:<8} : {:<15}'.format('Birthday', self.birthday) + '\n'
+#         rec += '\t {:<8}...{:<15}'.format('........', '...............') + '\n'
+#         rec += '\t {:<8} : {:<15}'.format('Number', self.phone) + '\n'
+#         rec += '\t {:<8}...{:<15}'.format('........', '...............') + '\n'
+#         return rec
+#
+#
+# fake = Faker()
+#
+#
+# def fake_records(book: ContactBook):
+#     for i in range(50):
+#         name = fake.first_name()
+#         date = fake.date_between(start_date='-70y', end_date='-15y')
+#         date_birth = datetime.strftime(date, '%Y-%m-%d')
+#         phone = str(fake.random_number(digits=randint(10, 15)))
+#         rec = Record(name=name, phone=phone, birthday=date_birth)
+#         book.add(rec)
+#     return book
+#
+#
+# book = fake_records(ContactBook())
+# # book.display_all()
+# book.search('Brian')
--- tests/test_contact_book.py
+++ tests/test_contact_book.py
@@ -1,6 +1,6 @@
 import pytest
 from contact_book.contactbook import ContactBook
-from unittest.mock import Mock, MagicMock, PropertyMock
+from unittest.mock import MagicMock, PropertyMock
 
 
 @pytest.fixture(name="contact_book")
@@ -28,8 +28,30 @@ def test_contact_book_delete(contact_book, record):
     assert res is True
 
 
+def test_contact_book_delete_if_no_data(contact_book):
+    res = contact_book.delete('name')
+    assert len(contact_book.names) == 0
+    assert res is False
+
+
 def test_contact_book_str(contact_book, record):
     contact_book.add(record)
-    msg = f'My contacts: \n name'
+    msg = f'My contacts: \nname\n'
     assert contact_book.__str__() == msg
 
+
+def test_contact_book_str_if_no_data(contact_book):
+    assert contact_book.__str__() is None
+
+
+def test_search(contact_book, record):
+    contact_book.add(record)
+    rec = contact_book.search('name')
+    assert rec == record
+    assert len(contact_book.names) == 1
+
+
+def test_search_if_no_data(contact_book):
+    rec = contact_book.search('name')
+    assert rec is None
+    assert len(contact_book.names) == 0
