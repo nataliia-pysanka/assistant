2b87f62de94c3a6ef3083e394fe300379ea0fefa
nataliia-pysanka <pysankanataliia@gmail.com>
2022-06-25 18:05:16 +0300

    add files for menu, main_menu

--- contact_book/record.py
+++ contact_book/record.py
@@ -1,2 +1,61 @@
+from datetime import datetime
+
+
+class Field:
+
+    def __init__(self, value: str) -> None:
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, data):
+        try:
+            self._value = datetime.strptime(data, '%d.%m.%Y').date()
+        except:
+            raise ValueError(
+                "Birthday format issue. Please enter birthday in DD.MM.YY format")
+
+    def days_to_birthday(self):
+        if not self.b_day:
+            return None
+        day_now = datetime.now().date()
+        current_year = self.b_day.param.replace(year=day_now.year)
+        if current_year > day_now:
+            delta = current_year - day_now
+            print(f'You have left {delta.days} to next birthday!')
+            return delta
+        else:
+            next_b_day = current_year.replace(year=day_now.year + 1)
+            delta = next_b_day - day_now
+            print(f'You have left {delta.days} to next birthday!')
+            return delta
+
+
 class Record:
-    pass
+    """
+    Class for instance Record
+    """
+
+    def __init__(self, name, birthday, phone):
+        self.name = name
+        self.birthday = birthday
+        self.phone = phone
+
+    def __str__(self):
+        rec = '\t {:<8}...{:<15}'.format('........', '...............') + '\n'
+        rec += '\t {:<8} : {:<15}'.format('Name', self.name) + '\n'
+        rec += '\t {:<8} : {:<15}'.format('Birthday', self.birthday) + '\n'
+        rec += '\t {:<8}...{:<15}'.format('........', '...............') + '\n'
+        rec += '\t {:<8} : {:<15}'.format('Number', self.phone) + '\n'
+        rec += '\t {:<8}...{:<15}'.format('........', '...............') + '\n'
+        return rec
--- main_menu.py
+++ main_menu.py
@@ -1,18 +1,23 @@
-from contact_book\contact_book import *
-from note_book\note_book import *
+from contact_book.contactbook_menu import contact_book_main
+from note_book.note_book import note_book_main
+
 
 def initial_main():
     while True:
         main_user_input = input('To enter contactbook please enter "1"\n'
-                            'To enter notebook please enter "2"\n'
-                            'To mantain your files please enter "3"\n'
-                            'To exit program please enter "4"\n')
+                                'To enter notebook please enter "2"\n'
+                                'To mantain your files please enter "3"\n'
+                                'To exit program please enter "4"\n')
         match main_user_input:
-          case '1': # Entering contactbook content
-            contact_book_main()
-          case '2': # Entering notebook content
-            note_book_main()
-          case '3': # Entering file mantaining content
-            pass
-          case '4': # Terminating programm
-           break
+            case '1':  # Entering contactbook content
+                contact_book_main()
+            case '2':  # Entering notebook content
+                note_book_main()
+            case '3':  # Entering file mantaining content
+                pass
+            case '4':  # Terminating programm
+                break
+
+
+if __name__ == '__main__':
+    initial_main()
