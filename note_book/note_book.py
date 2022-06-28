f59c65e2fd9e6efe50ed09bf935b8fbdfe8cf32b
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-06-28 21:17:27 +0300

    update branch

--- note_book/note_book.py
+++ note_book/note_book.py
@@ -116,6 +116,42 @@ class Note:
         else:
             print(f'\t The tag {tag} is not found \n')
 
+    # def delete_note(self, arg):
+    # """
+    # Delete specific note cand validation
+    # """
+    #     notebook = NoteBook()
+    #     if arg in notebook.data.keys():
+    #         del notebook.data[arg]
+    #         print(f'Note deleted')
+    #         notebook.save()
+    #     else:
+    #         print("Couldn't find the note)\n")
+
+    # def edit_note(self, text: Text, new_txt: Text):
+    # """
+    # Change note content and validation
+    # :param text: Text, new_txt: Text
+    # """
+    #     if not isinstance(text, Text):
+    #         print('\t Note not found, please enter correctly \n')
+    #         return
+
+    #     note_in_list = self.number_in_list(text)
+    #     if note_in_list:
+    #         note_in_list.value = new_txt.value
+    #         return
+    #     raise ValueError('\t The note was not found \n')
+
+    # def note_in_list(self, text: Text):
+    # """
+    # Get note position for edit_note function
+    # :param text: Text
+    # """
+    #     for pl in self.text:
+    #         if text.value == pl.value:
+    #             return pl
+
     def __str__(self):
         return f'Note: {self.name.value}'
 
@@ -212,6 +248,37 @@ class NoteBook(UserDict):
     def delete(self, note: Note):
         if note.name in self.data:
             del self.data[note.name]
+
+    # def edit_note(self, name: Name, new_note: Note) -> str:
+    # """
+    # Edit note content notebook
+    # :param name: Name, new_note: Note
+    # """
+    #     self.data[name] = new_note
+
+    # def find(self, param: str):
+    # """
+    # Search notes by user input and validation
+    # :param param: str
+    # """
+    #     if len(param) < 3:
+    #         raise ValueError("Param for find must be eq or grater than 3 symbols.")
+    #
+    #     notebook = NoteBook()
+    #
+    #     for k, v in self.items():
+    #         if param.lower() in k.lower() or [p.value for p in v.data if param in p.value]:
+    #             notebook.add(v)
+    #             continue
+    #     return notebook
+
+    # def sort_by_tag(self, tag: Tag):
+    # """
+    # Sort notes by tags
+    # :param tag: Tag
+    # """
+    #     notebook = NoteBook()
+    #     sorted_tags = sorted(notebook, key=itemgetter('tag'))
 
     def display_all(self):
         """
